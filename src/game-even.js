580c1e1845bc39c8f967c9a51a156382853b3d2f
Dmitry Skladchikov <dsklvdchikov@mail.ru>
2024-02-03 21:09:04 +0300

    game-even refactoring

--- src/game-even.js
+++ src/game-even.js
@@ -1,51 +1,28 @@
-import readLineSync from 'readline-sync';
+import * as game from './index.js';
 
-console.log('Welcome to the Brain Games!');
-const playerName = readLineSync.question('May I have your name? ');
-
-const playerGreeting = () => {
-  const greeting = `Hello, ${playerName}`;
-
-  return greeting;
-};
-
-const getRandomNumber = () => {
-  const maxRandomNum = 20;
-  const minRandomNum = 1;
-  const randomNum = Math.round(Math.random() * (maxRandomNum - minRandomNum) + minRandomNum);
-
-  return randomNum;
-};
+const rules = 'Answer "yes" if the number is even, otherwise answer "no".';
 
 const gameEven = () => {
-  console.log(playerGreeting());
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  const name = game.userName();
+  console.log(game.playerGreeting(name, rules));
 
-  let correctAnswers = 0;
-  let isCorrectAnswer = true;
+  let isCorrect = true;
+  let startsNewRound = true;
 
   do {
-    const currentRundomNumber = getRandomNumber();
+    const currentRundomNumber = game.getRandomNumber(1, 20);
     const isNumEven = currentRundomNumber % 2 === 0;
     const expextedAnswer = isNumEven ? 'yes' : 'no';
 
-    console.log(`Question: ${currentRundomNumber}`);
+    game.question(currentRundomNumber);
 
-    const userAnswer = readLineSync.question('Your answer: ');
-    const isCorrect = (isNumEven && userAnswer === 'yes') || (!isNumEven && userAnswer === 'no');
+    const userAnswer = game.userAnswer();
 
-    if (isCorrect) {
-      correctAnswers += 1;
-      console.log('Correctt!');
-    } else {
-      console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${expextedAnswer}`);
-      isCorrectAnswer = false;
-    }
-  } while (isCorrectAnswer && correctAnswers < 3);
+    isCorrect = game.isCorrectAnswer(userAnswer, expextedAnswer);
+    startsNewRound = game.startsRound();
+  } while (isCorrect && startsNewRound);
 
-  const message = correctAnswers === 3 ? `Congratulations, ${playerName}` : `Let's try again, ${playerName}`;
-
-  console.log(message);
+  console.log(game.finalMessage(name));
 };
 
 export default gameEven;
