3c58bd4d23e228e178897d060f35610e7576631d
Dmitry Skladchikov <dsklvdchikov@mail.ru>
2024-02-25 22:30:22 +0300

    adds runEngine function

--- src/index.js
+++ src/index.js
@@ -1,52 +1,28 @@
 import readLineSync from 'readline-sync';
+import { playerGreeting, getUserName } from './cli.js';
 
-export const userName = () => readLineSync.question('Welcome to the Brain Games!\nMay I have your name? ');
+const roundsCount = 3;
 
-export const playerGreeting = (name, rules) => `Hello, ${name}\n${rules}`;
+const runEngine = (rules, generateRound) => {
+  playerGreeting();
+  const name = getUserName();
+  console.log(rules);
 
-export const getRandomNumber = (min, max) => Math.round(Math.random() * (max - min) + min);
+  for (let i = 0; i < roundsCount; i += 1) {
+    const [question, answer] = generateRound();
+    console.log(`Question: ${question}`);
+    const userAnswer = readLineSync.question('Your answer: ');
 
-export const userAnswer = () => readLineSync.question('Your answer: ');
-
-export const question = (arg) => console.log(`Question: ${arg}`);
-
-// переменная задает количество раундов в игре
-let numberOfRounds = 3;
-
-// переменная хранит результат последнего из ответов
-let lastAnswerIsCorrect = true;
-
-// функция проверяет сходится ли ответ пользователя с ожидаемым
-export const isCorrectAnswer = (answer, expexted) => {
-  if (answer === expexted) {
-    console.log('Correctt!');
-  } else {
-    lastAnswerIsCorrect = false;
-    console.log(`'${answer}' is wrong answer ;(. Correct answer was '${expexted}'.`);
+    if (userAnswer === answer) {
+      console.log('Correct!');
+    } else {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${answer}'.`);
+      console.log(`Let's try again, ${name}!`);
+      return;
+    }
   }
-  return answer === expexted;
+
+  console.log(`Congratulations, ${name}!`);
 };
 
-// функция считающая нужно ли начинать новый раунд
-export const startsRound = () => {
-  numberOfRounds -= 1;
-  return numberOfRounds > 0;
-};
-
-// формирует сообщение по итогу игры
-export const finalMessage = (name) => {
-  const isFinalRound = numberOfRounds === 0;
-  const message = (isFinalRound && lastAnswerIsCorrect) ? `Congratulations, ${name}!` : `Let's try again, ${name}!`;
-  return message;
-};
-
-// возвращает рандомый элемент переданного массива
-export const randomElement = (arr) => {
-  const lengthOFArr = arr.length;
-  const min = 0;
-  const max = lengthOFArr - 1;
-
-  const randomInedex = Math.round(Math.random() * (max - min) + min);
-
-  return arr[randomInedex];
-};
+export default runEngine;
